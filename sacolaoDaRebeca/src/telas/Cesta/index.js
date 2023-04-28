a3a130709129debb83e80d3251184e486cedd66c
Rebeca Spinoza <101652283+RebecaSpinoza@users.noreply.github.com>
2023-04-28 19:45:58 -0300

    Update index.js

--- sacolaoDaRebeca/src/telas/Cesta/index.js
+++ sacolaoDaRebeca/src/telas/Cesta/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Descricao from './componentes/Descricao.js';
 import Topo from './componentes/Topo.js';
 import Itens from './componentes/itens.js';
+import { ScrollView } from 'react-native';
 
 export default function Cesta({topo, detalhes, itens}) {
-    return <>
+    return <ScrollView>
         <Topo {...topo}/>
         <Descricao {...detalhes}/>
         <Itens{...itens}/>
-    </>
+    </ScrollView>
 }
